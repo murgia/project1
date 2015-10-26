5ab1ef527361f08d6b82ae497610e1162098573c
Matthew Murgia <matthew.murgia@gmail.com>
2015-10-26 14:37:30 -0400

    added random functionality and next question button

--- scripts.js
+++ scripts.js
@@ -1,28 +1,28 @@
 // Declare variables for correct and incorrect count
 var correctCount = 0;
 var incorrectCount = 0;
-var questionNumber = 0;
 
 var questions = [
-  question1 = {
+  {
     question: "question1?",
     answer: "answer1"
   },
-  question2 = {
+  {
     question: "question2?",
     answer: "answer2"
   },
-  question3 = {
+  {
     question: "question3?",
     answer: "answer3"
   },
 ];
+
 // add click events to start, submit, next question, and reset
 $(".start").click(function(){
   console.log("testing start");
-  start();
+  getQuestion(getRandom(0,2));
 });
-
+// submit answer event
 $("#answer").click(function(evt){
   evt.preventDefault();
   console.log("testing submission");
@@ -31,6 +31,7 @@ $("#answer").click(function(evt){
 
 $("#nextQuestion").click(function(){
   console.log("testing next question");
+  getQuestion(getRandom(0,2));
 });
 
 $("#reset").click(function(){
@@ -42,16 +43,22 @@ var getRandom = function(min,max){
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 // create start function that moves to a random question ID and turns off start click event
-var start = function(){
+var getQuestion = function(questionNumber){
   $(".start").text(questions[questionNumber].question);
+  $(".answer").text(questions[questionNumber].answer);
   $(".start").off();
 };
 
 
 // create a check answer function that checks if the answer is correct
 var checkAnswer= function(){
-  if($("#answer-field").val() === questions[questionNumber].answer){
+  if($("#answer-field").val() == $(".answer").text()){
     console.log("test correct");
+    correctCount ++;
+    $("#correct-counter").html(correctCount);
   } else {
-    console.log("test incorrect");}
+    console.log("test incorrect");
+    incorrectCount ++;
+    $("#incorrect-counter").html(incorrectCount);
+  }
 };
